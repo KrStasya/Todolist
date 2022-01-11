b39c8d84e9c2e84bfa9b20f6cc4cd799349d56c9
KrStasya <anastasiya.krautsova@gmail.com>
2022-01-11 15:07:44 +0300

    add configureStore

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import { combineReducers} from "redux";
 import { todolistReducer} from "./todolist-reducer";
 import {tasksReducer} from "./tasks-reducer";
 import thunk from "redux-thunk";
 import {appReducer} from "./app-reducer";
 import {authReducer} from "./auth-reducer.ts";
+import {configureStore} from "@reduxjs/toolkit";
 
 
 const rootReducer=combineReducers({
@@ -15,8 +16,12 @@ const rootReducer=combineReducers({
 
 export type AppRootType = ReturnType<typeof rootReducer>
 
-export const store=createStore(rootReducer, applyMiddleware(thunk))
-
+export const store = configureStore({
+    reducer:rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware()
+            .prepend(thunk)
+})
 // @ts-ignore
 window.store=store
 
